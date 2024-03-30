25c62578cd802c7940129f6b224263a7e1c7b845
Tyr Chen <tyr.chen@gmail.com>
2024-03-30 10:41:52 -0700

    feature: support rw lock & display

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -23,7 +23,7 @@ fn main() -> Result<()> {
 
     loop {
         thread::sleep(Duration::from_secs(2));
-        println!("{:?}", metrics.snapshot());
+        println!("{}", metrics);
     }
 }
 
--- src/metrics.rs
+++ src/metrics.rs
@@ -4,12 +4,13 @@
 use anyhow::{anyhow, Result};
 use std::{
     collections::HashMap,
-    sync::{Arc, Mutex},
+    fmt,
+    sync::{Arc, RwLock},
 };
 
 #[derive(Debug, Clone)]
 pub struct Metrics {
-    data: Arc<Mutex<HashMap<String, i64>>>,
+    data: Arc<RwLock<HashMap<String, i64>>>,
 }
 
 impl Default for Metrics {
@@ -21,12 +22,12 @@ impl Default for Metrics {
 impl Metrics {
     pub fn new() -> Self {
         Metrics {
-            data: Arc::new(Mutex::new(HashMap::new())),
+            data: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 
     pub fn inc(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.lock().map_err(|e| anyhow!(e.to_string()))?;
+        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
         let counter = data.entry(key.into()).or_insert(0);
         *counter += 1;
         Ok(())
@@ -35,8 +36,18 @@ impl Metrics {
     pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
         Ok(self
             .data
-            .lock()
+            .read()
             .map_err(|e| anyhow!(e.to_string()))?
             .clone())
+    }
+}
+
+impl fmt::Display for Metrics {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let data = self.data.read().map_err(|_e| fmt::Error {})?;
+        for (key, value) in data.iter() {
+            writeln!(f, "{}: {}", key, value)?;
+        }
+        Ok(())
     }
 }
