5ea969f38bc9b786ed803df73e3c6e7adce8d681
Tyr Chen <tyr.chen@gmail.com>
2024-03-30 10:34:31 -0700

    fix: do not use unwrap

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -14,11 +14,11 @@ fn main() -> Result<()> {
     println!("{:?}", metrics.snapshot());
 
     for idx in 0..N {
-        task_worker(idx, metrics.clone()); // Metrics {data: Arc::clone(&metrics.data)}
+        task_worker(idx, metrics.clone())?; // Metrics {data: Arc::clone(&metrics.data)}
     }
 
     for _ in 0..M {
-        request_worker(metrics.clone());
+        request_worker(metrics.clone())?;
     }
 
     loop {
@@ -27,23 +27,34 @@ fn main() -> Result<()> {
     }
 }
 
-fn task_worker(idx: usize, metrics: Metrics) {
-    thread::spawn(move || loop {
-        // do long term stuff
-        let mut rng = rand::thread_rng();
+fn task_worker(idx: usize, metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            // do long term stuff
+            let mut rng = rand::thread_rng();
 
-        thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
-        metrics.inc(format!("call.thread.worker.{}", idx)).unwrap();
+            thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
+            metrics.inc(format!("call.thread.worker.{}", idx))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
     });
+    Ok(())
 }
 
-fn request_worker(metrics: Metrics) {
-    thread::spawn(move || loop {
-        // process requests
-        let mut rng = rand::thread_rng();
+fn request_worker(metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            // process requests
+            let mut rng = rand::thread_rng();
 
-        thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
-        let page = rng.gen_range(1..5);
-        metrics.inc(format!("req.page.{}", page)).unwrap();
+            thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
+            let page = rng.gen_range(1..5);
+            metrics.inc(format!("req.page.{}", page))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
     });
+
+    Ok(())
 }
